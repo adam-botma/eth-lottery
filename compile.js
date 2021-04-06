adece000bc085ff0cc67432cdef45feb9ca6da60
Adam Botma <adam.botma@me.com>
2021-04-05 21:18:14 -0700

    added mocha testing to make sure contract is working correctly

--- compile.js
+++ compile.js
@@ -4,8 +4,8 @@ const fs = require ('fs');
 const solc = require('solc');
 
 //import .sol file
-const inboxPath = path.resolve(__dirname , 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname , 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 //compile .sol file
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -18,4 +18,30 @@ contract Lottery {
 
   }
 
+//create random number
+function random() private view returns (uint) {
+    return uint(sha3(block.difficulty, now, players));
+  }
+
+//pick winner
+function  pickWinner() public  restricted{
+
+    uint index = random() % players.length;
+    players[index].transfer(this.balance);
+    players = new address[](0);
+
+  }
+
+
+//function modifier - used to not have to repeat ourselves in multiple functions - adds other functions where the underscore is
+modifier restricted() {
+    require(msg.sender == manager);
+    _;
+}
+
+function getPlayers () public view returns (address[]) {
+    return players;
+}
+
+
 }
